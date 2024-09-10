5383879b0fbf4f8cc77ed0d66afd0dfb9ac2cf01
MWIHAKI NGIGI <ngigileah92@gmail.com>
2024-09-10 18:10:55 +0300

    Update

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from tkinter import *
+from PIL import ImageTk
 class Login:
     def __init__(self, root):
-        pass
+        self.root = root
+        self.root.title("Login System")
+        self.root.geometry("1199x600+100+50")
+
+        # Background Image
+        self.bg=ImageTk.PhotoImage(file="images/1.jpg")
+        self.bg_image=Label(self.root, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
 
 
 root = Tk()
